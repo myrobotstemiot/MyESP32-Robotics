fae5bb1fd7e2d8ea2ea72db12e6e47a517dd3c32
Thong Toryod <87599310+myrobotstemiot@users.noreply.github.com>
2021-07-18 16:42:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     name : 'MyESP32',
     platform : 'arduino-esp32',
     title : 'MyESP32',
-    description : 'MyESP32.\n',
+    description : 'MyESP32-Robotics&IoT บอร์ดเพื่อการเรียนรู้ด้าน coding โดยใช้หุ่นยนต์และIoT เป็นสื่อกลางในการเรียนรู้\n',
     author : 'kruthong',
     website : 'http://www.myrobotstem.com/',
     email : 'myrobotstem@gmail.com',
